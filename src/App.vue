cf026ca64e1539d2031d9eb384009a0be3eea80b
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-05-24 12:50:37 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+        <h1>To Do -lista</h1>
+        <ToDoForm @todo-added="addToDo"></ToDoForm>
+        <ul>
+          <li v-for="item in ToDoItems" :key="item.id">
+            <ToDoItem 
+              :label="item.label"
+              :done="item.done"
+              :id="item.id"></ToDoItem>
+          </li>
+        </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import uniqueId from "lodash.uniqueid";
+import ToDoItem from "./components/ToDoItem.vue";
+import ToDoForm from "./components/TodoForm.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoItem,
+    ToDoForm
+  },
+  data() {
+    return {
+      ToDoItems : [
+        { id: uniqueId("todo-"), label: "Opiskele Vue.js", done: false },
+        { id: uniqueId("todo-"), label: "Tee eka Vue.js app", done: true },
+        { id: uniqueId("todo-"), label: "Pidä kivaa", done: true },
+        { id: uniqueId("todo-"), label: "Tee To Do -lista", done: false },
+        { id: uniqueId("todo-"), label: "Ota opintopisteet kotiin", done: false },
+      ]
+    }
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({ id: uniqueId("todo-"), label: toDoLabel, done: false })
+    }
   }
 }
 </script>
